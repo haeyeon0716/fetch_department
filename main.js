33861c44d4a9afc75581aaf0de6b9c62f1a87bf8
yhy7805 <yhy7805@gmail.com>
2023-09-22 10:09:54 +0900

    이미지 스타일링

--- main.js
+++ main.js
@@ -2,13 +2,8 @@
 const section = document.querySelector('section')
 
 fetch('DB/department.json')
-	.then((data) => {
-        //첫번째 then 구문에 받아진 promise를 다시 바로 리턴하면
-        //두번째 then구문에서 해당 데이터를 동기적으로 활용가능 
-		return data.json();
-	})
+	.then((data) => data.json())
     .then((json) => {
-        console.log(json.members);
         let tags = '';
         json.members.map(data => {
             tags += `
@@ -16,6 +11,8 @@ fetch('DB/department.json')
                 <div class = 'pic'>
                 <img src='img/${data.pic}'>
                 </div>
+                <h2>${data.name}</h2>
+                <p>${data.position}</p>
             </article>
             `;
         });
