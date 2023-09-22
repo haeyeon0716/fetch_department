fdb01c342eeeaeab657b89ff32929a0d6b3f5be9
yhy7805 <yhy7805@gmail.com>
2023-09-22 12:40:46 +0900

    hover  효과 모션 처리

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-
-const section = document.querySelector('section')
+const section = document.querySelector('section');
 
 fetch('DB/department.json')
 	.then((data) => data.json())
-    .then((json) => {
-        let tags = '';
-        json.members.map(data => {
-            tags += `
-            <article>
-                <div class = 'pic'>
-                <img src='img/${data.pic}'>
-                </div>
-                <h2>${data.name}</h2>
-                <p>${data.position}</p>
-            </article>
-            `;
-        });
+	.then((json) => {
+		let tags = '';
+		json.members.map((data) => {
+			tags += `
+        <article>
+          <div class='pic'>
+            <img src='img/${data.pic}' >
+            <img src='img/${data.pic}' >
+          </div>
+          <h2>${data.name}</h2>
+          <p>${data.position}</p>
+        </article>
+      `;
+		});
 
-        section.innerHTML = tags;
-    })
+		section.innerHTML = tags;
+	})
 	.catch((err) => {
 		console.log(err);
 	});
-
-   
